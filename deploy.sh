2c586481183470e385f92fe4ab95a383cfe289fd
Maarten van Schaik <maarten@informaarten.nl>
2013-12-22 14:07:46 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+CURDIR=$( cd "$(dirname "$0")" ; pwd -P )
+
+if [ ! -e ~/.gitconfig ]; then
+    if git version | grep "1.7" > /dev/null; then
+        ln -s $CURDIR/.gitconfig.1.7 ~/.gitconfig
+    else
+        ln -s $CURDIR/.gitconfig.1.8 ~/.gitconfig
+    fi
+    echo "Deployed .gitconfig"
+fi
+
+if [ ! -e ~/.vimrc ]; then
+    ln -s $CURDIR/.vimrc ~/.vimrc
+    mkdir -p ~/.vim/backup
+    echo "Deployed .vimrc"
+fi
+
+if [ ! -e ~/.screenrc ]; then
+    ln -s $CURDIR/.screenrc ~/.screenrc
+    echo "Deployed .screenrc"
+fi
